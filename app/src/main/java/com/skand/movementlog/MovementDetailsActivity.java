daf9ce5218c8f82910f2cc5bab69df640156969c
aravindsk <sk86.aravind@gmail.com>
2017-07-02 19:14:35 +0530

    tst

--- app/src/main/java/com/skand/movementlog/MovementDetailsActivity.java
+++ app/src/main/java/com/skand/movementlog/MovementDetailsActivity.java
@@ -176,7 +176,7 @@ public class MovementDetailsActivity extends AppCompatActivity {
         markerTimeStampList = new String[markerArraySize];
         polylineLatLngList = new String[exerciseLogList.size()-1];
 
-//Initialize start point - sample git test:123: checkedout SkLocaldev branch: 19:11update
+//Initialize start point - sample git test:123: checkedout SkLocaldev branch: 19:11update2
         latLngList = Arrays.asList(exerciseLogList.get(0)._lat_lng.split(","));
         locationStartPoint.setLatitude(Double.parseDouble(latLngList.get(0)));
         locationStartPoint.setLongitude(Double.parseDouble(latLngList.get(1)));
